61a0c39290d4b97c9f3d72746e5fc40bae117056
Dragan Pepic <d.pepic@hotmail.com>
2021-10-22 10:28:35 +0200

    Add project files.

---
+++ Validacija/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Validacija
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			DataContext = new Polaznik();
+		}
+
+		private void Sdf(object sender, RoutedEventArgs e)
+		{
+			var validator = new PolaznikValidator();
+			var rez = validator.Validate(DataContext as Polaznik);
+			if (!rez.IsValid)
+				foreach (var greska in rez.Errors)
+					MessageBox.Show($"{greska.PropertyName} -- {greska.ErrorMessage}");
+		}
+	}
+}
---
+++ Validacija/Polaznik.cs
@@ -0,0 +1,78 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validacija
+{
+	public class Polaznik : INotifyPropertyChanged, IDataErrorInfo
+	{
+		private string _tekst;
+		public string Tekst 
+		{
+			get => _tekst;
+			set
+			{
+				_tekst = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Tekst"));
+			}
+		}
+
+		private int _broj;
+		public int Broj 
+		{ 
+			get => _broj; 
+			set
+			{
+				_broj = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Broj"));
+			}
+		}
+
+		public string Error => throw new NotImplementedException();
+
+		private PolaznikValidator _validator = new();
+
+		public string this[string columnName]
+		{
+			get
+			{
+				/*switch (columnName)
+				{
+					case "Tekst":
+						if (_tekst is not null && _tekst.Length <= 3)
+							return "Premaaalo";
+						break;
+					case "Broj":
+						if (_broj is < 1 or > 10)
+							return "Nije ok :/";
+							break;
+				}*/
+				var err = _validator.Validate(this)
+					.Errors.Where(greska => greska.PropertyName == columnName)
+					.FirstOrDefault();
+				if (err is null)
+					return string.Empty;
+				else
+					return err.ErrorMessage;
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+
+	public class PolaznikValidator : AbstractValidator<Polaznik>
+	{
+		public PolaznikValidator()
+		{
+			RuleFor(pol => pol.Tekst)
+				.NotEmpty().WithMessage("Praaazno :/")
+				.MinimumLength(4).WithMessage("Premalo :/");
+			RuleFor(pol => pol.Broj)
+				.ExclusiveBetween(0, 11).WithMessage("Neeee");
+		}
+	}
+}
